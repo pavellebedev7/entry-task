dc8f15837a586c72f8f714b31e1cfa2ddfd5111d
Павел <pavellebedev7@gmail.com>
2020-02-29 22:10:54 +0300

    Clean version

--- sound recognition.py
+++ sound recognition.py
@@ -51,13 +51,6 @@ def rec0():
     global input0
     global LEN_0
     LEN_0 = FS * REC_TIME_0
-    # x = np.linspace(0, LEN_0, LEN_0)
-    # noise = np.random.normal(0, 0.02, LEN_0)
-    # z = noise + 0.5 * sp.sin(440.0 / FS * 2 * sp.pi * x + PHASE) + 0.5 * sp.sin(
-    #     880.0 / FS * 2 * sp.pi * x) + 0.5 * sp.sin(1760.0 / FS * 2 * sp.pi * x)
-    # input0 = z
-    # input0[:24255] = 0
-    # input0[26460:] = 0
 
     input0 = sd.rec(int(LEN_0), samplerate=FS, channels=1)
     sd.wait()
@@ -87,15 +80,6 @@ def play0():
 def rec1():
     global input1
     plt.close('all')
-    # x = np.linspace(0, LEN_1, LEN_1)
-    # z = 0.5 * sp.sin(440.0 / FS * 2 * sp.pi * x) + 0.5 * sp.sin(880.0 / FS * 2 * sp.pi * x) + 0.5 * sp.sin(
-    #     1760.0 / FS * 2 * sp.pi * x)
-    # input1 = z
-    # input1[:44100] = 0
-    # input1[1 * 44100:2 * 44100] = 0
-    # input1[3 * 44100:4 * 44100] = 0
-    # input1[6 * 44100:7 * 44100] = 0
-    # input1[8 * 44100:9 * 44100] = 0
 
     input1 = sd.rec(int(LEN_1), samplerate=FS, channels=1)
     sd.wait()
@@ -135,11 +119,9 @@ def play1():
     #  Spectrum calculation
     for i in range(0, n0):
         zf0[i] = 2.0 / CHUNK * abs(fft(y0[CHUNK * i:CHUNK * (i + 1)] * w)[0:HALF_CHUNK])
-        # zf0[i] = zf0[i] / np.amax(zf0[i])
         output0[i] = medfilt(zf0[i], MEDFILT_W)
     for i in range(0, n1):
         zf1[i] = 2.0 / CHUNK * abs(fft(y1[CHUNK * i:CHUNK * (i + 1)] * w)[0:HALF_CHUNK])
-        # zf1[i] = zf1[i] / np.amax(zf1[i])
         output1[i] = medfilt(zf1[i], MEDFILT_W)
 
     #  Spectrum filter
@@ -178,15 +160,11 @@ def play1():
 
     #  Input/output plotting
     plt.figure()
-    plt.subplot(211)
+    plt.title('Blue - first record, orange - second record, green - overlap')
     plt.plot(x0 / FS, y0, x1 / FS, y1, xd * n1 / ((n - 1) * N), diff)
     plt.grid(True)
     plt.fill_between(xd * n1 / ((n - 1) * N), -1, 1, where=pred > 0, color='green', alpha='0.75')
-
-    plt.subplot(212)
-    plt.plot(xf0 * N, medfilt(zf0[0], MEDFILT_W), xf1 * N, medfilt(zf1[0], MEDFILT_W))
-    plt.xscale('log')
-    plt.grid(True)
+    plt.xlabel('Time, s')
     plt.show()
 
 
